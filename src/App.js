fc35e2d55d3da1ef1c247e9b897912934141fb89
Akash <akashharikumar2013@gmail.com>
2022-07-13 21:29:11 +0530

    Started React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './Components/Button.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = () => {
+    return ( 
+        <div>
+            Hello World
+        </div>
+     );
+}
+ 
+export default Button;
