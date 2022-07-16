b5d4027c7475051b070681790ca262db228b808e
Akash <akashharikumar2013@gmail.com>
2022-07-16 21:27:01 +0530

    Designed the Task List and Task Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar.jsx';
+import TaskList from './Components/Tasks/TaskList';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <br/>
+      <br/>
+      <TaskList />
     </div>
   );
 }
